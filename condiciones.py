d237a99d8f50ec1496d14805ab786e35df0ba65b
Henry <john_henry.4m@hotmail.com>
2023-08-18 10:17:13 -0500

    NT3 Se inicia condicionales

---
+++ condiciones.py
@@ -0,0 +1,7 @@
+edadUsuario=int(input("Digita tu edad: "))
+
+if edadUsuario>= 18:
+    print("Bienvenido a la disco")
+else:
+    print("Eres menor de edad, pa la casa")
+    print("Oe pa la casa....")    
